87e929d256d07d9026297b2f119cd4bdeabe1913
badripaudel77 <badri@eventlogic.se>
2022-12-12 21:40:35 +0545

    Pipe to sort the ingredients created and used in template.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HeaderComponent} from "./header/header.component";
+import { HeaderComponent } from "./header/header.component";
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipe-list/recipes-list.component';
 import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 import { RecipesItemComponent } from './recipes/recipe-list/recipes-item/recipes-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import {DropdownDirective} from "./directives/dropdown.directive";
-import {RecipeService} from "./services/recipe.service";
-import {ShoppingListService} from "./services/shopping-list.service";
+import { DropdownDirective } from "./directives/dropdown.directive";
+import { RecipeService } from "./services/recipe.service";
+import { ShoppingListService } from "./services/shopping-list.service";
 import { NoRecipeSelectedComponent } from './recipes/no-recipe-selected/no-recipe-selected.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { SortIngredientsByName} from "./pipes/sort-by-alphabet.pipe";
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     ShoppingEditComponent,
     DropdownDirective,
     NoRecipeSelectedComponent,
-    RecipeEditComponent
+    RecipeEditComponent,
+    SortIngredientsByName
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pipes/sort-by-alphabet.pipe.ts
@@ -0,0 +1,20 @@
+import {Pipe, PipeTransform} from "@angular/core";
+import {IngredientModel} from "../common/Ingredient.model";
+
+/**
+ * Pipes are used to transform data like uppercase, lowercase, sorting etc.
+ * It needs to be declared in app module.
+ * Used for formatting to data display on UI.
+ * Identified by @Pipe({..}) decorator, other it is just a normal class that implements PipeTransform
+ * REF : https://angular.io/guide/pipes-custom-data-trans
+ */
+@Pipe({
+  name: 'sortIngredientsByName'
+})
+export class SortIngredientsByName implements PipeTransform {
+  transform(ingredients: IngredientModel[], property:string = 'name'): IngredientModel[] {
+    return ingredients.sort((a, b) => {
+      return a[property].localeCompare(b[property]);
+    });
+  }
+}
