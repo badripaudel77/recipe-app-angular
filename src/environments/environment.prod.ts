d2c30e6f516407dcf27fd6c23e4d6b5f8059174e
badripaudel77 <badri@eventlogic.se>
2022-12-17 15:40:24 +0545

    config related firebase variables migrated to environment files. gitignore file updated.

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  FIREBASE_BASE_URL : 'https://ng-app-e6a4b-default-rtdb.firebaseio.com',
-  //FIREBASE_SIGNUP_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=[API_KEY]',
-  // query params will be appended later in the request
-  FIREBASE_SIGNUP_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:signUp',
-  FIREBASE_SIGNIN_URL: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword',
-  FIREBASE_AUTH_API_KEY: 'AIzaSyC2HFOQeQ9eh_-fTZhWk85pxiP0tiDpaL4'
-};
